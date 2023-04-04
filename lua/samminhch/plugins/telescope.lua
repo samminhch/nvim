ab65fae154d1a224189bed3e68640b6f838241b6
samminhch@forg <45446622+samminhch@users.noreply.github.com>
2023-04-04 13:15:02 -0500

    Added keybind descriptions for telescope.lua

--- lua/samminhch/plugins/telescope.lua
+++ lua/samminhch/plugins/telescope.lua
@@ -4,12 +4,12 @@ return {
     version = false,
     cmd = 'Telescope',
     keys = {
-        {'<leader>sf', '<cmd>Telescope find_files<cr>', '[S]earch [F]iles'},
-        {'<leader>sr', '<cmd>Telescope oldfiles<cr>', '[S]earch [R]ecent'},
-        {'<leader>sg', '<cmd>Telescope git_files<cr>', '[S]earch [G]it files'},
-        {'<leader>sw', '<cmd>Telescope grep_string<cr>', '[S]earch [W]ord in file'},
-        {'<leader>sd', '<cmd>Telescope diagnostics<cr>', '[S]earch [D]iagnostics'},
-        {'<leader>sb', '<cmd>Telescope buffers<cr>', '[S]earch [B]uffers'}
+        { '<leader>sf', '<cmd>Telescope find_files<cr>',  desc = '[S]earch [F]iles' },
+        { '<leader>sr', '<cmd>Telescope oldfiles<cr>',    desc = '[S]earch [R]ecent' },
+        { '<leader>sg', '<cmd>Telescope git_files<cr>',   desc = '[S]earch [G]it files' },
+        { '<leader>sw', '<cmd>Telescope grep_string<cr>', desc = '[S]earch [W]ord in file' },
+        { '<leader>sd', '<cmd>Telescope diagnostics<cr>', desc = '[S]earch [D]iagnostics' },
+        { '<leader>sb', '<cmd>Telescope buffers<cr>',     desc = '[S]earch [B]uffers' }
     },
     dependencies = {
         'nvim-lua/plenary.nvim',
