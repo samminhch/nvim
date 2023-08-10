fe7d42c9f71711d1fc7b127c562c55613751534a
Minh Nguyen <45446622+samminhch@users.noreply.github.com>
2023-08-10 17:42:34 -0500

    Added nvim-dap support for `C`, `C++`, and `Rust`

--- lua/samminhch/plugins/lsp.lua
+++ lua/samminhch/plugins/lsp.lua
@@ -3,14 +3,15 @@ return {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v2.x',
         config = function()
-          require('lsp-zero.settings').preset({})
+            require('lsp-zero.settings').preset({})
         end
     },
     {
-        "folke/neodev.nvim", opts = {
-            library = { 
+        "folke/neodev.nvim",
+        opts = {
+            library = {
                 plugins = { "nvim-dap-ui" },
-                types = true 
+                types = true
             }
         }
     },
@@ -48,8 +49,6 @@ return {
             { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
             { 'mfussenegger/nvim-jdtls' },
-            { 'mfussenegger/nvim-dap' },
-            { 'rcarriga/nvim-dap-ui'}
         },
         config = function()
             local lsp = require('lsp-zero').preset({})
@@ -104,8 +103,74 @@ return {
             lsp.skip_server_setup({ 'jdtls' })
 
             lsp.setup()
-
-            require('dapui').setup()
         end
     },
+    -- debugging
+    {
+        "mfussenegger/nvim-dap",
+        dependencies = {
+            { "rcarriga/nvim-dap-ui" },
+            { "theHamsta/nvim-dap-virtual-text" },
+            { "nvim-telescope/telescope-dap.nvim" },
+            { "jbyuki/one-small-step-for-vimkind" },
+        },
+        keys = {
+            { '<leader>dt',  function() require('dapui').toggle() end,          desc = '[D]ebugger [T]oggle' },
+            { '<leader>db',  function() require('dap').toggle_breakpoint() end, desc = '[D]ebugger Toggle [B]reakpoint' },
+            { '<leader>dr',  function() require('dap').run_to_cursor() end,     desc = '[D]ebugger [R]un to Cursor' },
+            { '<leader>dc',  function() require('dap').continue() end,          desc = '[D]ebugger [C]ontinue' },
+            { '<leader>dp',  function() require('dap').pause.toggle() end,      desc = '[D]ebugger Toggle [P]ause' },
+            { '<leader>dR',  function() require('dap').repl.toggle() end,       desc = '[D]ebugger Toggle [R]EPL' },
+            { '<leader>dsi', function() require('dap').step_into() end,         desc = '[D]ebugger [S]tep [I]nto' },
+            { '<leader>dso', function() require('dap').step_over() end,         desc = '[D]ebugger [S]tep [O]ver' },
+            { '<leader>dsO', function() require('dap').step_out() end,          desc = '[D]ebugger [S]tep [O]ut' },
+            { '<leader>dsO', function() require('dap').step_out() end,          desc = '[D]ebugger [S]tep [O]ut' },
+        },
+        config = function(plugin, opts)
+            require("nvim-dap-virtual-text").setup {
+                commented = true,
+            }
+
+            local dap, dapui = require('dap'), require('dapui')
+            dapui.setup()
+
+            dap.listeners.after.event_initialized["dapui_config"] = function()
+                dapui.open()
+            end
+            dap.listeners.before.event_terminated["dapui_config"] = function()
+                dapui.close()
+            end
+            dap.listeners.before.event_exited["dapui_config"] = function()
+                dapui.close()
+            end
+
+            local codelldb_install = require('mason-registry').get_package('codelldb'):get_install_path()
+
+            dap.adapters.codelldb = {
+                type = 'server',
+                port = '${port}',
+                executable = {
+                    command = codelldb_install .. '/extension/adapter/codelldb',
+                    args = { '--port', '${port}' },
+                    detached = vim.fn.has('win32') ~= 1
+                }
+            }
+
+            dap.configurations.cpp = {
+                {
+                    name = "Launch file",
+                    type = "codelldb",
+                    request = "launch",
+                    program = function()
+                        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+                    end,
+                    cwd = '${workspaceFolder}',
+                    stopOnEntry = false,
+                },
+            }
+
+            dap.configurations.c = dap.configurations.cpp
+            dap.configurations.rust = dap.configurations.cpp
+        end
+    }
 }
