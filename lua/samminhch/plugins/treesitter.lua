025a5d655aaa2b6d79505dfe869fae8b9ce6fd0b
Minh Nguyen <45446622+samminhch@users.noreply.github.com>
2023-09-23 12:28:06 -0500

    Added playground feature in treesitter

--- lua/samminhch/plugins/treesitter.lua
+++ lua/samminhch/plugins/treesitter.lua
@@ -24,6 +24,9 @@ return {
                 end
             end,
         },
+        {
+            'nvim-treesitter/playground'
+        }
     },
     opts = {
         highlight = { enable = true },
@@ -59,6 +62,24 @@ return {
                     ['[c'] = '@class.outer'
                 }
             }
+        },
+        playground = {
+            enable = true,
+            disable = {},
+            updatetime = 25,         -- Debounced time for highlighting nodes in the playground from source code
+            persist_queries = false, -- Whether the query persists across vim sessions
+            keybindings = {
+                toggle_query_editor = 'o',
+                toggle_hl_groups = 'i',
+                toggle_injected_languages = 't',
+                toggle_anonymous_nodes = 'a',
+                toggle_language_display = 'I',
+                focus_language = 'f',
+                unfocus_language = 'F',
+                update = 'R',
+                goto_node = '<cr>',
+                show_help = '?',
+            },
         },
     },
     config = function(_, opts)
