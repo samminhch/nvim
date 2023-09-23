d9b182bd073abac4b0c8cacb52b06e3d7fb62ebb
Minh Nguyen <45446622+samminhch@users.noreply.github.com>
2023-09-23 13:18:52 -0500

    Updated plugins and formatted utils/init.lua

--- lua/samminhch/utils/init.lua
+++ lua/samminhch/utils/init.lua
@@ -2,17 +2,17 @@ local utils = {}
 
 ---@param plugin string
 function utils.has(plugin)
-    return require('lazy.core.config').plugins[plugin]~=nil
+    return require('lazy.core.config').plugins[plugin] ~= nil
 end
 
 ---@param fn fun()
 function utils.on_very_lazy(fn)
-  vim.api.nvim_create_autocmd('User', {
-    pattern = 'VeryLazy',
-    callback = function()
-      fn()
-    end,
-  })
+    vim.api.nvim_create_autocmd('User', {
+        pattern = 'VeryLazy',
+        callback = function()
+            fn()
+        end,
+    })
 end
 
 function utils.check_back_space()
@@ -25,7 +25,7 @@ function utils.map(mode, binding, value, options)
 end
 
 function utils.nmapd(binding, value, description)
-    utils.map('n', binding, value, {desc = description})
+    utils.map('n', binding, value, { desc = description })
 end
 
 return utils
