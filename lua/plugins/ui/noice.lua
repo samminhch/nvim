c4c2d6afc4df806ec9dd9e5e459d8bd29c998c69
samminhch@stemtop <45446622+samminhch@users.noreply.github.com>
2024-01-02 17:45:21 -0600

    moved nui.lua into noice.lua as a dep

--- lua/plugins/ui/noice.lua
+++ lua/plugins/ui/noice.lua
@@ -1,21 +1,22 @@
 return {
-    'folke/noice.nvim',
-    event = 'VeryLazy',
+    "folke/noice.nvim",
+    event = "VeryLazy",
     cond = not vim.g.vscode,
+    dependencies = {
+        {
+            "MunifTanjim/nui.nvim",
+            lazy = true,
+            cond = not vim.g.vscode
+        }
+    },
     opts = {
         lsp = {
-            signature = {
-                enabled = false
-            },
             -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
             override = {
                 ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
                 ["vim.lsp.util.stylize_markdown"] = true,
                 ["cmp.entry.get_documentation"] = true,
             },
-            hover = {
-                enabled = false
-            }
         },
         -- you can enable a preset for easier configuration
         presets = {
@@ -24,9 +25,6 @@ return {
             long_message_to_split = true, -- long messages will be sent to a split
             inc_rename = false,           -- enables an input dialog for inc-rename.nvim
             lsp_doc_border = true,        -- add a border to hover docs and signature help
-        },
-        hover = {
-            enabled = false,
         },
     }
 }
