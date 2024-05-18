95b172943a1d41fff2ceb1950942d2a0288e8c73
samminhch@minhtop <45446622+samminhch@users.noreply.github.com>
2024-05-18 16:52:28 -0500

    Added which-key labels for keybind groups

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -2,8 +2,15 @@ return {
     'folke/which-key.nvim',
     enabled = not vim.g.vscode,
     config = function()
-        vim.o.timeout = true
-        vim.o.timeoutlen = 300
         require('which-key').setup()
+        -- labeling keybind chains
+        require('which-key').register {
+            ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+            ['<leader>b'] = { name = '[B]arbar', _ = 'which_key_ignore' },
+            ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+            ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+            ['<leader>f'] = { name = '[F]ile', _ = 'which_key_ignore' },
+            ['<leader>d'] = { name = '[D]ebugger', _ = 'which_key_ignore' },
+        }
     end
 }
