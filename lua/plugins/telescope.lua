94768c5bfbaa25d4acb7f9b6db069114ab6c5068
samminhch@minhtop <45446622+samminhch@users.noreply.github.com>
2024-06-11 19:18:45 -0500

    Updated colorschemes and telescope configs

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,79 +1,67 @@
 return {
-    {
-        "nvim-telescope/telescope.nvim",
-        cond = not vim.g.vscode,
-        version = false,
-        cmd = "Telescope",
-        keys = function()
-            local builtin = require("telescope.builtin")
-            return {
-                { "<leader>sf", builtin.find_files,  desc = "[S]earch [F]iles" },
-                { "<leader>sr", builtin.oldfiles,    desc = "[S]earch [R]ecent" },
-                { "<leader>sg", builtin.git_files,   desc = "[S]earch [G]it files" },
-                { "<leader>sw", builtin.grep_string, desc = "[S]earch [W]ord in file" },
-                { "<leader>sd", builtin.diagnostics, desc = "[S]earch [D]iagnostics" },
-                { "<leader>sb", builtin.buffers,     desc = "[S]earch [B]uffers" },
-                { "<leader>sn", builtin.notify,      desc = "[S]earch [N]otifications" },
-                { "<leader>sk", builtin.keymaps,     desc = "[Search] [K]eymaps" },
-                { "<leader>sh", builtin.help_tags,   desc = "[Search] [H]elp Files" },
-                { "<leader>sm", builtin.man_pages,   desc = "[Search] [M]an Pages" },
-            }
-        end,
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "debugloop/telescope-undo.nvim",
-            "nvim-telescope/telescope-ui-select.nvim",
-        },
-        opts = {
-            defaults = {
-                vimgrep_arguments = {
-                    "rg",
-                    "-L",
-                    "--no-heading",
-                    "--with-filename",
-                    "--line-number",
-                    "--column",
-                    "--smart-case",
-                },
-                prompt_prefix = "   ",
-                selection_caret = "  ",
-                entry_prefix = "  ",
-                initial_mode = "insert",
-                selection_strategy = "reset",
-                sorting_strategy = "ascending",
-                layout_strategy = "horizontal",
-                layout_config = {
-                    horizontal = {
-                        prompt_position = "top",
-                        preview_width = 0.55,
-                        results_width = 0.8,
-                    },
-                    vertical = {
-                        mirror = false,
-                    },
-                    width = 0.87,
-                    height = 0.80,
-                    preview_cutoff = 120,
-                },
-                file_ignore_patterns = { "node_modules" },
-                path_display = { "truncate" },
-                winblend = 0,
-                border = {},
-                borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-                color_devicons = true,
-                set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
-            },
-            extensions = {
-                ["ui-select"] = {
-                    require('telescope.themes').get_dropdown(),
-                }
-            }
+    "nvim-telescope/telescope.nvim",
+    cond = not vim.g.vscode,
+    version = false,
+    cmd = "Telescope",
+    keys = function()
+        local builtin = require("telescope.builtin")
+        return {
+            { "<leader>sf", builtin.find_files,  desc = "[S]earch [F]iles" },
+            { "<leader>sr", builtin.oldfiles,    desc = "[S]earch [R]ecent" },
+            { "<leader>sg", builtin.git_files,   desc = "[S]earch [G]it files" },
+            { "<leader>sw", builtin.grep_string, desc = "[S]earch [W]ord in file" },
+            { "<leader>sd", builtin.diagnostics, desc = "[S]earch [D]iagnostics" },
+            { "<leader>sb", builtin.buffers,     desc = "[S]earch [B]uffers" },
+            { "<leader>sn", builtin.notify,      desc = "[S]earch [N]otifications" },
+            { "<leader>sk", builtin.keymaps,     desc = "[S]earch [K]eymaps" },
+            { "<leader>sh", builtin.help_tags,   desc = "[S]earch [H]elp Files" },
+            { "<leader>sm", builtin.man_pages,   desc = "[S]earch [M]an Pages" },
+            { "<leader>sc", builtin.colorscheme, desc = "[S]earch [C]olorschemes" }
         }
-    },
-    {
+    end,
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "debugloop/telescope-undo.nvim",
         "nvim-telescope/telescope-ui-select.nvim",
-        config = function()
-            require("telescope").load_extension("ui-select")
-        end
+    },
+    opts = {
+        defaults = {
+            vimgrep_arguments = {
+                "rg",
+                "-L",
+                "--no-heading",
+                "--with-filename",
+                "--line-number",
+                "--column",
+                "--smart-case",
+            },
+            prompt_prefix = "   ",
+            selection_caret = "  ",
+            entry_prefix = "  ",
+            initial_mode = "insert",
+            selection_strategy = "reset",
+            sorting_strategy = "ascending",
+            layout_strategy = "horizontal",
+            layout_config = {
+                horizontal = {
+                    prompt_position = "top",
+                    preview_width = 0.55,
+                    results_width = 0.8,
+                },
+                vertical = {
+                    mirror = false,
+                },
+                width = 0.87,
+                height = 0.80,
+                preview_cutoff = 120,
+            },
+            file_ignore_patterns = { "node_modules" },
+            path_display = { "truncate" },
+            winblend = 0,
+            border = {},
+            borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+            color_devicons = true,
+            set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+        },
     }
 }
--- lua/plugins/ui/colorschemes.lua
+++ lua/plugins/ui/colorschemes.lua
@@ -1,42 +1,25 @@
 return {
-    "runih/colorscheme-picker.nvim",
-    cond = not vim.g.vscode,
-    dependencies = {
-        "nvim-telescope/telescope.nvim",
-        {
-            "sainnhe/everforest",
-            lazy = false,
-            priority = 1000,
-            cond = not vim.g.vscode,
-            config = function()
-                -- colorscheme options
-                vim.g.everforest_background = "hard"
-                vim.g.everforest_disable_italic_comment = true
-                vim.g.everforest_better_performance = true
-                vim.g.everforest_sign_column_background = "grey"
-                vim.g.everforest_transparent_background = 1
+    {
+        "sainnhe/everforest",
+        lazy = false,
+        priority = 1000,
+        cond = not vim.g.vscode,
+        config = function()
+            -- colorscheme options
+            vim.g.everforest_background = "hard"
+            vim.g.everforest_disable_italic_comment = true
+            vim.g.everforest_better_performance = true
+            vim.g.everforest_sign_column_background = "grey"
 
-                vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-                vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+            vim.g.everforest_transparent_background = (vim.g.neovide and 0 or 1)
+            vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
-                -- load the colorscheme
-                vim.cmd.colorscheme("everforest")
-            end
-        },
-        { "catppuccin/nvim",  name = "catppuccin", priority = 1000, cond = not vim.g.vscode },
-        { "dracula/vim",      name = "dracula",    priority = 1000, cond = not vim.g.vscode },
-        { "rktjmp/lush.nvim", name = "lush",       priority = 1000, cond = not vim.g.vscode }
-    },
-    config = function()
-        local ok, colorscheme = pcall(require, "colorscheme-picker")
-        if not ok then
-            print("Color Picker is not loaded")
-            return
+            -- load the colorscheme
+            vim.cmd.colorscheme("everforest")
         end
-        colorscheme.setup({
-            default_colorscheme = "everforest",
-            keymapping = "<leader>sc",
-        })
-        colorscheme.set_default_colorscheme()
-    end,
+    },
+    { "catppuccin/nvim",  name = "catppuccin", priority = 1000, cond = not vim.g.vscode },
+    { "dracula/vim",      name = "dracula",    priority = 1000, cond = not vim.g.vscode },
+    { "rktjmp/lush.nvim", name = "lush",       priority = 1000, cond = not vim.g.vscode }
 }
