fd30438c6f760a6a2f7a6ded1d987c43586173be
samminhch@minhtop <45446622+samminhch@users.noreply.github.com>
2024-06-11 19:13:10 -0500

    Updated dependencies for debugging plugins

--- lua/plugins/coding/debugging.lua
+++ lua/plugins/coding/debugging.lua
@@ -3,6 +3,7 @@ return {
     cond = not vim.g.vscode,
     dependencies = {
         "rcarriga/nvim-dap-ui",
+        "nvim-neotest/nvim-nio",
         "jay-babu/mason-nvim-dap.nvim",
         "theHamsta/nvim-dap-virtual-text",
         "nvim-telescope/telescope-dap.nvim",
