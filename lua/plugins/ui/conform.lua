fd8e890d4286f4278d1bf4e6e6575342e2283d06
samminhch@minhtop <45446622+samminhch@users.noreply.github.com>
2024-07-13 22:10:07 -0500

    Redid formatter options to use vim's shiftwidth on all configured formatters

--- lua/plugins/ui/conform.lua
+++ lua/plugins/ui/conform.lua
@@ -12,46 +12,60 @@ return {
             desc = "[F]ormat Buffer",
         },
     },
-    opts = {
-        format_on_save = function(bufnr)
-            if slow_format_filetypes[vim.bo[bufnr].filetype] then
-                return
-            end
+    opts = function()
+        local shiftwidth = vim.api.nvim_get_option_value("shiftwidth", {})
 
-            local function on_format(err)
-                if err and err:match("timeout$") then
-                    slow_format_filetypes[vim.bo[bufnr].filetype] = true
+        return {
+            format_on_save = function(bufnr)
+                if slow_format_filetypes[vim.bo[bufnr].filetype] then
+                    return
                 end
-            end
 
-            return { timeout_ms = 1000, lsp_format = "fallback" }, on_format
-        end,
-        format_after_save = function(bufnr)
-            if not slow_format_filetypes[vim.bo[bufnr].filetype] then
-                return
-            end
-            return { lsp_format = "fallback" }
-        end,
-        formatters_by_ft = {
-            lua = { "stylua" },
-            rust = { "rustfmt" },
-            tex = { "latexindent" },
-            python = { "isort", "black" },
-        },
-        formatters = {
-            stylua = {
-                prepend_args = {
-                    "--collapse-simple-statement",
-                    "FunctionOnly",
-                    "--sort-requires",
-                    "--quote-style",
-                    "AutoPreferDouble",
-                    "--indent-type",
-                    "Spaces",
-                    "--indent-width",
-                    vim.api.nvim_get_option_value("shiftwidth", {}),
+                local function on_format(err)
+                    if err and err:match("timeout$") then
+                        slow_format_filetypes[vim.bo[bufnr].filetype] = true
+                    end
+                end
+
+                return { timeout_ms = 1000, lsp_format = "fallback" }, on_format
+            end,
+            format_after_save = function(bufnr)
+                if not slow_format_filetypes[vim.bo[bufnr].filetype] then
+                    return
+                end
+                return { lsp_format = "fallback" }
+            end,
+            formatters_by_ft = {
+                json = { "prettierd" },
+                lua = { "stylua" },
+                rust = { "rustfmt" },
+                tex = { "latexindent" },
+                python = { "isort", "black" },
+                markdown = { "prettierd" },
+            },
+            -- stylua: ignore start
+            formatters = {
+                shfmt = {
+                    prepend_args = { "-i", shiftwidth },
+                },
+                stylua = {
+                    prepend_args = {
+                        "--collapse-simple-statement", "FunctionOnly",
+                        "--sort-requires",
+                        "--quote-style", "AutoPreferDouble",
+                        "--indent-type", "Spaces",
+                        "--indent-width", shiftwidth,
+                    },
+                },
+                prettierd = {
+                    prepend_args = {
+                        "--tab-width", shiftwidth,
+                        "--no-semi", "false",
+                        "--trailing-comma", "none",
+                    },
                 },
             },
-        },
-    },
+            -- stylua: ignore end
+        }
+    end,
 }
