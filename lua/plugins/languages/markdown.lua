8e1213f94f4c4298f9e65f8fcc8a0706ceb20774
samminhch@minhtop <45446622+samminhch@users.noreply.github.com>
2024-07-13 22:07:56 -0500

    Started custom css for markdown preview!

--- lua/plugins/languages/markdown.lua
+++ lua/plugins/languages/markdown.lua
@@ -4,9 +4,15 @@ return {
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
     build = function() vim.fn["mkdp#util#install"]() end,
-    config = function ()
+    config = function()
         vim.g.mkdp_preview_options = {
-            sync_scroll_type = "relative"
+            sync_scroll_type = "relative",
         }
-    end
+
+        vim.g.mkdp_markdown_css = vim.fn.expand("~/.config/nvim/lua/plugins/languages/markdown.css")
+
+        vim.g.mkdp_page_title = "${name}"
+
+        vim.g.mkdp_theme = "light"
+    end,
 }
