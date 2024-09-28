d49d60b1cf71c2dce9a25ab2c11e578b07cef3da
samminhch@minhtop <45446622+samminhch@users.noreply.github.com>
2024-09-28 18:23:33 -0500

    Updated nvim-jdtls config

--- lua/plugins/languages/java.lua
+++ lua/plugins/languages/java.lua
@@ -1,35 +1,32 @@
 return {
     "mfussenegger/nvim-jdtls",
     ft = "java",
-    config = function()
+    enabled = false,
+    opts = function()
+        local capabilities = require("cmp_nvim_lsp").default_capabilities()
         local bundles = {
             vim.fn.glob(
                 require("mason-registry").get_package("java-debug-adapter"):get_install_path()
-                .. "/extension/server/com.microsoft.java.debug.plugin-*.jar",
+                    .. "/extension/server/com.microsoft.java.debug.plugin-*.jar",
+                true
+            ),
+            vim.fn.glob(
+                require("mason-registry").get_package("java-test"):get_install_path() .. "/extension/server/*.jar",
                 true
             ),
         }
 
-        vim.list_extend(bundles,
-            vim.split(
-                vim.fn.glob(
-                    require("mason-registry").get_package("java-test"):get_install_path()
-                    .. "/extension/server/*.jar", true
-                ),
-                "\n"
-            )
-        )
-
-        require("jdtls").start_or_attach({
+        return {
             cmd = { "jdtls" },
-            root_dir = vim.fs.dirname(
-                vim.fs.find(
-                    { "gradlew", ".git", "mvnw" },
-                    { upward = true })[1]
-            ),
+            root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
             init_options = {
-                bundles = bundles
-            }
-        })
-    end
+                bundles = bundles,
+            },
+            capabilities = capabilities,
+        }
+    end,
+    config = function(opts)
+        require("jdtls").setup_dap({ hotcodereplace = "auto" })
+        require("jdtls").start_or_attach(opts)
+    end,
 }
