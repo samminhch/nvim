f0e482e31dcded6402dc921c5c39d07057eb66a3
samminhch@minhtop <45446622+samminhch@users.noreply.github.com>
2024-09-28 18:24:08 -0500

    updated colorscheme / status bar config

--- lua/plugins/ui/colorschemes.lua
+++ lua/plugins/ui/colorschemes.lua
@@ -11,15 +11,15 @@ return {
             vim.g.everforest_better_performance = true
             vim.g.everforest_sign_column_background = "grey"
 
-            vim.g.everforest_transparent_background = (vim.g.neovide and 0 or 1)
+            -- vim.g.everforest_transparent_background = (vim.g.neovide and 0 or 1)
             vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
             vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
             -- load the colorscheme
             vim.cmd.colorscheme("everforest")
-        end
+        end,
     },
-    { "catppuccin/nvim",  name = "catppuccin", priority = 1000, cond = not vim.g.vscode },
-    { "dracula/vim",      name = "dracula",    priority = 1000, cond = not vim.g.vscode },
-    { "rktjmp/lush.nvim", name = "lush",       priority = 1000, cond = not vim.g.vscode }
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000, cond = not vim.g.vscode },
+    { "dracula/vim", name = "dracula", priority = 1000, cond = not vim.g.vscode },
+    { "rktjmp/lush.nvim", name = "lush", priority = 1000, cond = not vim.g.vscode },
 }
--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -1,13 +1,20 @@
+local function keymap()
+    if vim.opt.iminsert:get() > 0 and vim.b.keymap_name then
+        return "⌨ " .. vim.b.keymap_name
+    end
+    return ""
+end
+
 return {
-    'nvim-lualine/lualine.nvim',
+    "nvim-lualine/lualine.nvim",
     cond = not vim.g.vscode,
-    event = 'VeryLazy',
+    event = "VeryLazy",
     opts = {
         options = {
             icons_enabled = true,
-            theme = 'auto',
-            component_separators = { left = '', right = '' },
-            section_separators = { left = '', right = '' },
+            theme = "auto",
+            component_separators = { left = "", right = "" },
+            section_separators = { left = "", right = "" },
             disabled_filetypes = {
                 "neo-tree",
                 "undotree",
@@ -21,17 +28,17 @@ return {
                 statusline = 1000,
                 tabline = 1000,
                 winbar = 1000,
-            }
+            },
         },
         sections = {
-            lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 }, },
-            lualine_b = { 'branch', 'diff', 'diagnostics' },
-            lualine_c = { 'filename' },
-            lualine_x = { 'encoding', 'fileformat', 'filetype' },
-            lualine_y = { 'progress' },
+            lualine_a = { { "mode", separator = { left = "", right = "" } } },
+            lualine_b = { "branch", "diff", "diagnostics" },
+            lualine_c = { "filename" },
+            lualine_x = { "encoding", "fileformat", "filetype" },
+            lualine_y = { keymap, "progress" },
             lualine_z = {
-                { 'location', separator = { right = '' }, left_padding = 2 },
+                { "location", separator = { left = "", right = "" } },
             },
-        }
-    }
+        },
+    },
 }
