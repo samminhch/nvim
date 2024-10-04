6f3c7f0a2dd1c05aa7f59264965eb35a6bc3f780
samminhch@minhtop <45446622+samminhch@users.noreply.github.com>
2024-10-04 15:07:52 -0500

    Not using yadm for now, commenting out support in git-signs

--- lua/plugins/coding/git-signs.lua
+++ lua/plugins/coding/git-signs.lua
@@ -15,14 +15,14 @@ return {
         current_line_blame_opts = {
             delay = 0,
         },
-        yadm = {
-            enable = vim.fn.executable("yadm") == 1
-        },
+        -- yadm = {
+        --     enable = vim.fn.executable("yadm") == 1
+        -- },
         on_attach = function()
             local nmapd = require("utils").nmapd
             nmapd("<leader>gp", "<cmd>Gitsigns preview_hunk<cr>", "[G]itsigns [P]review Hunk")
             nmapd("<leader>gd", "<cmd>Gitsigns diffthis<cr>", "[G]itsigns [D]iff File")
             nmapd("<leader>gb", "<cmd>Gitsigns toggle_current_line_blame<cr>", "[G]itsigns [B]lame Toggle")
-        end
-    }
+        end,
+    },
 }
