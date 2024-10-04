dc6a458a59354388df04936e7715ebf832ba0392
samminhch@minhtop <45446622+samminhch@users.noreply.github.com>
2024-10-04 14:38:21 -0500

    Made git-signs skinnier

--- lua/plugins/coding/git-signs.lua
+++ lua/plugins/coding/git-signs.lua
@@ -3,12 +3,12 @@ return {
     enabled = not vim.g.vscode,
     opts = {
         signs = {
-            add          = { text = "█" },
-            change       = { text = "█" },
-            delete       = { text = "░" },
-            topdelete    = { text = "░" },
-            changedelete = { text = "░" },
-            untracked    = { text = "█" },
+            add = { text = "┃" },
+            change = { text = "┃" },
+            delete = { text = "┇" },
+            topdelete = { text = "┇" },
+            changedelete = { text = "┇" },
+            untracked = { text = "┃" },
         },
         attach_to_untracked = true,
         current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
